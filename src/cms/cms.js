11fafa295c7203c0ea1210af0b9d0163c948ab74
Ting <badonlineqq@gmail.com>
2020-10-10 14:16:31 +1100

    add configuration for cloudinary

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -10,6 +10,32 @@ import IndexPagePreview from './preview-templates/IndexPagePreview'
 CMS.registerMediaLibrary(uploadcare)
 CMS.registerMediaLibrary(cloudinary)
 
+CMS.init({
+    config: {
+        media_library: {
+            name: 'cloudinary',
+            config: {
+                cloud_name: 'dnrvhlddk',
+                api_key: '745536962636175',
+                default_transformations: [
+                    [
+                        {
+                            width: 2000,
+                            quality: 80,
+                            crop: 'limit',
+                        },
+                    ],
+                ],
+            },
+        },
+        media_folder: '/static/uploads',
+        public_folder: '/uploads',
+        collections: [
+
+        ]
+    },
+});
+
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
